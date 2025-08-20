6e5ac861a12abb7e5bc15e81469135ead73777df
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-08-20 21:20:08 +0500

    Hammad is here

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
@@ -25,6 +25,10 @@ function App() {
   function handleClick() {
     setIsDark(!isDark);
   }
+  useEffect(() => {
+    console.log(`Search Term: ${searchTerm}`);
+    console.log(`Filtered Items Count: ${filteredItems.length}`);
+  }, [searchTerm, filteredItems]);
   return (
     <div
       className={
